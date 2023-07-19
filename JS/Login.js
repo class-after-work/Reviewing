305dc70826736a47ec7c2ca5f09faccdb24cd3b2
Guheon2 <131875839+Guheon2@users.noreply.github.com>
2023-07-19 15:58:00 +0900

    Add files via upload
    복습용 파일~

---
+++ JS/Login.js
@@ -0,0 +1,29 @@
+const loginBtn = document.querySelector("#login-form button");
+const loginValue = document.querySelector("#login-form input");
+const form = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASS = "hidden";
+const USER_NAME = "names";
+
+function loginEvent(event) {
+  const username = loginValue.value;
+  event.preventDefault();
+  localStorage.setItem("USER_NAME", username);
+  form.classList.add(HIDDEN_CLASS);
+  showGreeting(username);
+}
+
+function showGreeting(tomato) {
+  greeting.classList.remove(HIDDEN_CLASS);
+  greeting.innerText = `Hello! ${tomato}`;
+}
+
+const savedName = localStorage.getItem("USER_NAME");
+
+if (savedName === null) {
+  form.classList.remove(HIDDEN_CLASS);
+  loginBtn.addEventListener("click", loginEvent);
+} else {
+  form.classList.add(HIDDEN_CLASS);
+  showGreeting(savedName);
+}
