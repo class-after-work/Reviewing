d5135d09d79f5024a0dbf07d8aa9d34d30a856e1
Guheon2 <131875839+Guheon2@users.noreply.github.com>
2023-07-25 19:14:48 +0900

    Add files via upload

--- JS/todo.js
+++ JS/todo.js
@@ -1,21 +1,48 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
+const TODOS_KEY = "todos";
+let arrayTodo = [];
+
+function deleteTodo(item) {
+  const li = item.target.parentElement;
+  li.remove();
+  arrayTodo = arrayTodo.filter((toDo) => toDo.id !== parseInt(li.id));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(arrayTodo));
+}
 
 function writeTodo(whatTodo) {
   const li = document.createElement("li");
+  li.id = whatTodo.id;
+  console.log(whatTodo.id);
   const span = document.createElement("span");
+  span.innerText = ` ${whatTodo.text}`;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteTodo);
   li.appendChild(span);
-  span.innerText = `I Need to ${whatTodo}`;
+  li.appendChild(btn);
   todoList.appendChild(li);
 }
 
 function todoSubmit(event) {
   event.preventDefault();
   const whatTodo = todoInput.value;
-  localStorage.setItem("todos", JSON.stringify(whatTodo));
   todoInput.value = "";
-  writeTodo(whatTodo);
+  const whatTodoObj = {
+    text: whatTodo,
+    id: Date.now(),
+  };
+  arrayTodo.push(whatTodoObj);
+  writeTodo(whatTodoObj);
+  localStorage.setItem(TODOS_KEY, JSON.stringify(arrayTodo));
 }
-
 todoForm.addEventListener("submit", todoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodo = JSON.parse(savedTodos);
+  arrayTodo = parsedTodo;
+  parsedTodo.forEach((item) => writeTodo(item));
+}
